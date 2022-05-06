f3872ca23a3ac76678ccfa1cac9f976db03d3eaf
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-06 10:50:08 -0700

    handle Auth

--- src/views/Login/Auth.jsx
+++ src/views/Login/Auth.jsx
@@ -1,15 +1,35 @@
 // login form
+import { signInUser, signUpUser } from "../../services/user";
 
-import React from 'react'
 
 export default function Auth() {
   // state: email, pass, location/history, newUser
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [newUser, setNewUser] = useState(false);
-  // handle: authSubmit, 
-  // return: form (email, pass), sign in/up button
+  const [error, setError] = useState('');
 
+  // handle: authSubmit, 
+  async function handleAuthSubmit(e) {
+    try {
+      e.preventDefault();
+      {
+        if (!newUser) {
+          const user = await signInUser(email, password);
+          setCurrentUser(user);
+        } else {
+          const user = await signUpUser(email, password);
+          setCurrentUser(user);
+        }
+        setEmail('');
+        setPassword('');
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  // return: form (email, pass), sign in/up button
   return (
     <>
       <form onSubmit={handleAuthSubmit}
@@ -42,7 +62,8 @@ export default function Auth() {
             !newUser
               ? 'Sign in'
               : 'Sign up'
-          }</button>
+          }
+        </button>
       </form>
     </>
   )
