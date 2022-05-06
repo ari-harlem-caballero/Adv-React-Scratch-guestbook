ecd2fd5b314f1b92b9a6fd198c3907fe01aab290
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-06 09:19:22 -0700

    list/auth pseudo

--- src/views/List/EntryList.jsx
+++ src/views/List/EntryList.jsx
@@ -2,6 +2,8 @@
 import React from 'react'
 
 export default function EntryList() {
+  // state: entries, loading
+  // return: logout button, loading, map entires
   return (
     <div>EntryList</div>
   )
--- src/views/Login/Auth.jsx
+++ src/views/Login/Auth.jsx
@@ -3,6 +3,10 @@
 import React from 'react'
 
 export default function Auth() {
+  // state: email, pass, location/history, newUser
+  // handle: authSubmit, 
+  // return: form (email, pass), sign in/up button
+
   return (
     <div>Auth</div>
   )
