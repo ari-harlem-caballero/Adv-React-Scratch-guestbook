6447aec4c43a87058e12ef9d2149a252910e7277
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-05-12 09:47:26 -0700

    setting up behavioral test

--- src/views/List/EntryList.test.jsx
+++ src/views/List/EntryList.test.jsx
@@ -1,1 +1,45 @@
 // behavior testing (load, display list, input/type)
+
+import { screen, render, waitForElementToBeRemoved } from "@testing-library/react";
+import userEvent from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom";
+import App from "../../App";
+import { UserProvider } from "../../context/UserContext";
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const server = setupServer();
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('App', () => {
+  it('should allow user to see their posts and add new entry', async () => {
+    render(
+      <MemoryRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </MemoryRouter>
+    );
+
+    const emailInput = screen.getByPlaceholderText('email');
+    userEvent.type(emailInput, 'test@example.com');
+
+    const passwordInput = screen.getByPlaceholderText('password');
+    userEvent.type(passwordInput, 'samplePassword');
+
+    const signInButton = screen.getByRole('button', { name: /sign in/i });
+    userEvent.click(signInButton);
+
+    screen.getByText('Page Loading');
+
+    await waitForElementToBeRemoved(screen.getByText('Page Loading'));
+
+    // // list load
+    await screen.findByText('Past entries:');
+    // form exists
+    // adds new item to list
+  });
+})
